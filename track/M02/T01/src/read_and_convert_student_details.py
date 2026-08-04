76bd6c285302103a92f7fa32c0627b502b2d19bb
Vaidik Patidar <vaidik11patidar@gmail.com>
2026-08-04 12:43:32 +0530

    Implement student input type conversion

---
+++ track/M02/T01/src/read_and_convert_student_details.py
@@ -0,0 +1,14 @@
+# Read and convert student details
+student_name = input()
+student_age = int(input())
+course_rating = float(input())
+
+# Display the values
+print(f"Student: {student_name}")
+print(f"Age: {student_age}")
+print(f"Rating: {course_rating}")
+
+# Display the data types
+print(type(student_name))
+print(type(student_age))
+print(type(course_rating))
