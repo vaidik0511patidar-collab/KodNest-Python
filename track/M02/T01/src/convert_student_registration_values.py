557f45e00bbcbc0b2d7ab6d5c2b6fa5496db4f0b
Vaidik Patidar <vaidik11patidar@gmail.com>
2026-08-04 12:19:48 +0530

    Added type conversion for student registration values

---
+++ track/M02/T01/src/convert_student_registration_values.py
@@ -0,0 +1,16 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+
+# Convert the values here
+age = int(age_text)
+course_fee = float(course_fee_text)
+attempts_text = str(attempts)
+
+# Display the converted values and their data types
+print(age)
+print(type(age))
+print(course_fee)
+print(type(course_fee))
+print(attempts_text)
+print(type(attempts_text))
