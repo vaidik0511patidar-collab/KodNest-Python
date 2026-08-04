dd39ab338f493418d22ca942c1ad5b37b1f2263a
Vaidik Patidar <vaidik11patidar@gmail.com>
2026-08-04 12:59:47 +0530

    Understanding the difference between division and floor division

---
+++ track/M02/T01/calculate_division_and_floor_division.py
@@ -0,0 +1,9 @@
+first_number = int(input())
+second_number = int(input())
+
+# Calculate division and floor division and then print both on separate lines
+division = first_number / second_number
+floor_division = first_number // second_number
+
+print(f"Division: {division}")
+print(f"Floor Division: {floor_division}")
