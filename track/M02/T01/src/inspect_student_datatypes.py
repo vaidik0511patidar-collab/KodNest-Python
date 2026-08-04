68c9b74c3eddd5da5047c2649c1d06bca015d97d
Vaidik Patidar <vaidik11patidar@gmail.com>
2026-08-04 12:06:48 +0530

    Identifying Data Types

---
+++ track/M02/T01/src/inspect_student_datatypes.py
@@ -0,0 +1,11 @@
+# Create the required variables
+student_name = "Vaidik"
+course_duration = 45
+course_rating = 4.6
+is_enrolled = True
+
+# Display the data type of each variable
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
