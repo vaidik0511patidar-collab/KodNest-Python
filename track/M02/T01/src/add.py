a0735c28d490fca60c6ed7091265f17123cabaca
Vaidik Patidar <vaidik11patidar@gmail.com>
2026-08-04 13:06:19 +0530

    Reading values from user, adding it and displaying the result

---
+++ track/M02/T01/src/add.py
@@ -0,0 +1,9 @@
+# Read the numbers
+first_number = int(input())
+second_number = int(input())
+
+# Calculate the sum
+result = first_number + second_number
+
+# Display the result
+print(f"Sum: {result}")
