53c96ab858fab1e9ca3463b90afc348d678b5631
Vaidik Patidar <vaidik11patidar@gmail.com>
2026-08-05 09:27:26 +0530

    Understanding conditional statements (if, elif, else)

---
+++ track/M02/T02/src/Check_positive_negative_or_zero.py
@@ -0,0 +1,10 @@
+# Read the number
+number = int(input())
+
+# Check whether the number is positive, negative or zero
+if number > 0:
+    print("Number is Positive")
+elif number < 0:
+    print("Number is Negative")
+else:
+    print("Number is Zero")
