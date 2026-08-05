f5f3ee1f6edacf462725acd5f9360bcf034c72be
Vaidik Patidar <vaidik0511patidar@gmail.com>
2026-08-05 15:11:29 +0530

    Display number sequence and word characters using for loop

---
+++ track/M02/T02/src/Display_a_number_sequence_and_word_characters.py
@@ -0,0 +1,13 @@
+# Read the number and word
+number = int(input())
+word = input()
+
+# Display the number sequence
+print("Numbers:")
+for i in range(1,number+1):
+    print(i)
+
+# Display the characters
+print("Characters:")
+for i in word:
+    print(i)
